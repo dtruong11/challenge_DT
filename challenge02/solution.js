011919a589754b114bc0a4064244a4805d428d17
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2019-02-10 17:17:07 -0800

    simplified

--- challenge02/solution.js
+++ challenge02/solution.js
@@ -11,70 +11,76 @@ const maxCampers = (content) => {
   let rows = info[0]
   let cols = info[1]
   let distance = info[2]
-  let visited = new Map()
+  let visited = []
   let tents = []
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (matrix[i][j] === 'X') visited[`${i}*${j}`] = true
-      else visited[`${i}*${j}`] = false
-
+      if (matrix[i][j] === 'X') {
+        visited[i] = []
+        visited[i][j] = true
+      }
+      else {
+        visited[i] = []
+        visited[i][j] = false
+      }
       if (matrix[i][j] === 'T') {
         tents.push({ row: i, col: j })
       }
     }
   }
 
+  console.log('tents', tents)
+
   let q = new Queue()
   let fCounter = 0
   let wCounter = 0
   let tent = tents.shift()
   q.add(tent)
-  visited.set({ row: tent.row, col: tent.col }, true)
+  visited[tent.row][tent.col] = true
   while (q.peek()) {
     let cur = q.pop()
     let { row, col } = cur
-    console.log('cur', cur)
     // Food found
-    if (matrix[cur.row][cur.col] === 'F') {
+    console.log('matrix[row][col]', row, col, matrix[row][col])
+    if (matrix[row][col] === 'F') {
       console.log('found food')
       return fCounter
     }
 
     // Water found 
-    if (matrix[cur.row][cur.col] === 'W') {
+    if (matrix[row][col] === 'W') {
       console.log('found water')
       return wCounter
     }
 
     // Moving up 
-    if (cur[row - 1] >= 0 && visited.get({ row: cur[row - 1], col: cur.col }) === false) {
-      q.add({ row: cur[row - 1], col: cur.col })
+    if (row - 1 >= 0 && visited[row - 1][col] === false) {
+      q.add({ row: row - 1, col: col })
       fCounter++
-      visited.set({ row: cur[row - 1], col: cur.col }, true)
+      visited[row - 1][col] = true
     }
 
     // Moving down 
-    if (cur[row + 1] < rows && visited.get({ row: cur[row + 1], col: cur.col } === false)) {
-      q.add({ row: cur[row + 1], col: cur.col })
+    if (row + 1 < rows && visited[row + 1][col] === false) {
+      q.add({ row: row + 1, col })
       fCounter++
-      visited.set({ row: cur[row + 1], col: cur.col }, true)
+      visited[row + 1][col] = true
     }
 
     // Moving left
-    if (cur[col - 1] >= 0 && visited.get({ row: cur.row, col: cur[col - 1] } === false)) {
-      q.add({ row: cur.row, col: cur[col - 1] })
+    if (col - 1 >= 0 && visited[row][col - 1] === false) {
+      q.add({ row: row, col: col - 1 })
       fCounter++
-      visited.set({ row: cur.row, col: cur[col - 1] }, true)
+      visited[row][col - 1] = true
     }
 
-    // Moving right 
-    if (cur[col + 1] >= 0 && visited.get({ row: cur.row, col: cur[col + 1] } === false)) {
-      q.add({ row: cur.row, col: cur[col + 1] })
+    // // Moving right 
+    if (col + 1 >= 0 && visited[row][col + 1] === false) {
+      q.add({ row: row, col: col + 1 })
       fCounter++
-      visited.set({ row: cur.row, col: cur[col + 1] }, true)
+      visited[row][col + 1] = true
     }
   }
-  console.log('fCounter', fCounter)
   return fCounter ? fCounter : 0
 }
 
